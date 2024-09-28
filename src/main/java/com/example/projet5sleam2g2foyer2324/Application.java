cf98783f2b00ab098a7924cc2f31fbe241ce9c81
bahaeddine.somrani <bahaeddine.somrani@esprit.tn>
2024-09-28 15:21:45 +0100

    Create Project

---
+++ src/main/java/com/example/projet5sleam2g2foyer2324/Application.java
@@ -0,0 +1,13 @@
+package com.example.projet5sleam2g2foyer2324;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
